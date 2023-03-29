9be87d2ae3eed8ee089d4415133a9b0d462a162f
Unidracorn <unidragorn@gmail.com>
2023-03-29 17:13:43 -0300

    requisito 1

---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,29 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare readonly id:number;
+  declare teamName:string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    field: 'team_name',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Team',
+  tableName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
